49a8aa4d0864eebdc8c6263c23b3971e3f742b16
Adam <adaskoo112@gmail.com>
2021-07-05 17:09:28 +0200

    Removed my IP address

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ from time import sleep
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.12"
+        self.server = "Your IP address"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
