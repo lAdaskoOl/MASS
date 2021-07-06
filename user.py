f79b2fb9d708920e3f78b8e18c6512d1e84dd3b7
Adam <adaskoo112@gmail.com>
2021-07-06 12:19:13 +0200

    updated user interface

--- user.py
+++ user.py
@@ -9,31 +9,65 @@ pg.display.set_caption("MASS (Mr. Adasko's Security System)")
 class UserInterface:
 
     def __init__(self, screen, width, height):
-        self.delay = 100
+        self.delay = 10
         self.screen = screen
         self.width = width
         self.height = height
         self.button = (self.width/2 - 100, self.height-100, 200, 80)
+        self.counter = 1
+
+    def not_connected(self):
+        self.screen.fill((255, 255, 255))
+
+        pg.draw.rect(self.screen, (0, 255, 0), self.button, 0)
+
+        pg.font.init()  # you have to call this at the start, if you want to use this module.
+        myfont = pg.font.SysFont('Times New Roman', 30)
+
+        textsurface = myfont.render('Connect', False, (0, 0, 0))
+        screen.blit(textsurface, (self.width / 2 - 50, self.height - 85))
+
+        x, y = pg.mouse.get_pos()
+        if x >= self.width / 2 - 100 and x <= self.width / 2 + 100 and y >= self.height - 100 and y <= self.height + 80:
+            pg.draw.rect(self.screen, (255, 255, 0), self.button, 0)
+            textsurface = myfont.render('Connect', False, (255, 0, 0))
+            screen.blit(textsurface, (self.width / 2 - 50, self.height - 85))
+
+    def connected(self):
+        self.screen.fill((255, 255, 255))
+
+        pg.draw.rect(self.screen, (0, 255, 0), self.button, 0)
+
+        pg.font.init()  # you have to call this at the start, if you want to use this module.
+        myfont = pg.font.SysFont('Times New Roman', 30)
+
+        textsurface = myfont.render('Disconnect', False, (0, 0, 0))
+        screen.blit(textsurface, (self.width / 2 - 65, self.height - 85))
+
+        x, y = pg.mouse.get_pos()
+        if x >= self.width / 2 - 100 and x <= self.width / 2 + 100 and y >= self.height - 100 and y <= self.height + 80:
+            pg.draw.rect(self.screen, (255, 255, 0), self.button, 0)
+            textsurface = myfont.render('Disconnect', False, (255, 0, 0))
+            screen.blit(textsurface, (self.width / 2 - 65, self.height - 85))
 
     def run(self):
 
         running = True
         while running:
-            self.screen.fill((255, 255, 255))
 
-            pg.draw.rect(self.screen, (0, 255, 0), self.button, 0)
+            if self.counter % 2 == 0:
+                self.connected()
+            else:
+                self.not_connected()
 
             x, y = pg.mouse.get_pos()
-            if x >= self.width / 2 - 100 and x <= self.width / 2 + 100 and y >= self.height - 100 and y <= self.height + 80:
-                pg.draw.rect(self.screen, (255, 255, 0), self.button, 0)
-
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
                 if x >= self.width/2-100 and x <= self.width/2+100 and y >= self.height-100 and y <= self.height+80:
                     if pg.mouse.get_pressed()[0]:
                         print("YOYOYO") #TODO: connect the interface to the socket server
-
+                        self.counter += 1
 
             pg.display.flip()
             pg.event.pump()
