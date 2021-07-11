ee4212a5399da5fc2df1d987177ae62dbdd7e02e
Adam <adaskoo112@gmail.com>
2021-07-11 09:37:46 +0200

    updated user interface

--- user.py
+++ user.py
@@ -5,7 +5,8 @@ from network import Network
 class User():
     def __init__(self):
         self.master = Tk()
-        self.master.geometry("300x400")
+        self.master.title("MASS")
+        self.master.geometry("230x400")
         self.master.resizable(False, False)
         self.master.protocol("WM_DELETE_WINDOW", self.close)
         self.app = Canvas(self.master)
@@ -13,17 +14,26 @@ class User():
         label = Label(bg="orange")
         label.place(x=0, y=0, width=300, height=400)
 
+        label_login = Label(text="Login: ", bg="orange")
+        label_login.place(x=10, y=10, width=100, height=25)
+
+        password_login = Label(text="Password: ", bg="orange")
+        password_login.place(x=10, y=55, width=100, height=25)
+
         self.login = Text()
-        self.login.place(x=0, y=0, width=100, height=25)
+        self.login.place(x=110, y=10, width=100, height=25)
 
         self.password = Text()
-        self.password.place(x=0, y=45, width=100, height=25)
+        self.password.place(x=110, y=55, width=100, height=25)
 
         self.button1 = Button(text='Connect', command=self.connect)
-        self.button1.place(x=0, y=90, width=100, height=50)
+        self.button1.place(x=10, y=120, width=100, height=50)
 
-        #self.button2 = Button(text="Disconnect", command=self.disconnect)
-        #self.button2.place(x=115, y=90, width=100, height=50)
+        self.button2 = Button(text='Get image')
+        self.button2.place(x=120, y=120, width=100, height=50)
+
+        self.info = Label(text="Not connected", bg="grey", fg="red")
+        self.info.place(x=10, y=200, width=210, height=180)
 
         self.n = Network()
         self.counter = 0
@@ -47,9 +57,11 @@ class User():
             self.app.update()
             if self.counter % 2 == 0:
                 self.button1['text'] = 'Connect'
+                self.info['text'] = "Not connected"
             else:
                 self.button1['text'] = 'Disconnect'
                 data = self.n.send("data")
+                self.info['text'] = data
                 #print(data)
             time.sleep(0.1)
 
